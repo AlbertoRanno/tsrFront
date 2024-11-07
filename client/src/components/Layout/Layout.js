2f7842b2cf52ce0db7bac7d32ad313d2bd9d7187
AlbertoRanno <albertoranno@hotmail.com>
2024-11-07 00:06:03 -0300

    Links activos

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,28 +1,51 @@
 // src/components/Layout/Layout.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Layout.css';
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname === path ? 'active-link' : '';
+    };
+
     return (
         <div className="layout">
             <header>
                 <nav>
                     <ul>
-                        <li><Link href="/user-preferences" className="nav-logo">
-                            TSR
-                        </Link></li>
-                        <li><Link to="/">Productos</Link></li>
-                        <li><Link to="/purchases">Compras</Link></li>
-                        <li><Link to="/sales">Ventas</Link></li>
-                        <li><Link to="/reports">Reportes</Link></li>
+                        <li>
+                            <Link to="/user-preferences" className="nav-logo">
+                                TSR
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className={isActive('/')}>
+                                Productos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/purchases" className={isActive('/purchases')}>
+                                Compras
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/sales" className={isActive('/sales')}>
+                                Ventas
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/reports" className={isActive('/reports')}>
+                                Reportes
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
             </header>
             <main>{children}</main>
             <footer>
-                <p>&copy; 2024 Tres Simples Razones. All rights reserved.
-                </p>
+                <p>&copy; 2024 Tres Simples Razones. All rights reserved.</p>
             </footer>
         </div>
     );
